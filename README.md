8984af594754cebac1880216776a73699640b1b3
Thaina Santos <thainados_santos@hotmail.com>
2024-08-22 23:14:41 -0300

    add pages

---
+++ README.md
@@ -0,0 +1,52 @@
+# Project Drawing Board
+
+Bem-vindo ao **Project Drawing Board**, um quadro de desenho interativo criado com JavaScript, HTML e CSS. Este projeto permite que os usuários desenhem diretamente no quadro usando uma paleta de cores e oferece a opção de limpar o quadro com um simples clique.
+
+## Funcionalidades
+
+- **Desenho Interativo**: Desenhe no quadro utilizando o mouse.
+- **Paleta de Cores**: Escolha entre várias cores disponíveis para criar desenhos variados.
+- **Botão Limpar**: Apague o conteúdo do quadro a qualquer momento com o botão "Limpar Quadro".
+- **Interface Simples e Intuitiva**: Interface amigável, desenvolvida com foco em facilidade de uso.
+
+## Tecnologias Utilizadas
+
+- **HTML5**
+- **CSS3**
+- **JavaScript**
+
+## Como Usar
+
+1. Clone este repositório:
+    ```bash
+    git clone https://github.com/seu-usuario/project-drawing-board.git
+    ```
+
+2. Navegue até o diretório do projeto:
+    ```bash
+    cd project-drawing-board
+    ```
+
+3. Abra o arquivo `index.html` no seu navegador:
+    ```bash
+    start index.html
+    ```
+
+4. Comece a desenhar no quadro, escolhendo a cor desejada na paleta. Para limpar o quadro, clique no botão "Limpar Quadro".
+
+## Captura de Tela
+
+<img src="assets/project-img.png" alt= "img_readme" >
+
+## Contribuição
+
+Contribuições são bem-vindas! Sinta-se à vontade para abrir um pull request ou reportar issues.
+
+## Licença
+
+[![NPM](https://img.shields.io/npm/l/react)](]https://github.com/ThainaSantoss/ThainaSantoss.github.io/blob/main/LICENSE)
+Este projeto está licenciado sob a licença MIT.
+
+---
+
+Criado por [Thainá dos Santos Silva](https://github.com/ThainaSantoss).
--- js/script.js
+++ js/script.js
@@ -1,72 +1,70 @@
-//inicial data
-let currentColor = 'black';
+//Inicial Data
+let currentColor = "black"; // selected color
 let canDraw = false;
 let mouseX = 0;
 let mouseY = 0;
 
-let screen = document.querySelector('#tela');
-let ctx = screen.getContext('2d');
+let screen = document.querySelector("#tela");
+let ctx = screen.getContext("2d"); // selecting canvas context
 
-
-//events
-document.querySelectorAll('.colorArea .color').forEach(item => {
-    item.addEventListener('click', colorClickEvent);
+//Events
+document.querySelectorAll(".colorArea .color").forEach((item) => {
+  item.addEventListener("click", colorClickEvent); // click event to use all colors
 });
 
-screen.addEventListener('mousedown', mouseDownEvent);
-screen.addEventListener('mousemove', mouseMoveEvent);
-screen.addEventListener('mouseup', mouseUpEvent);
-
-document.querySelector('.clear').addEventListener('click', clearScreen)
+// mouse events on screen (move, click and release)
+screen.addEventListener("mousedown", mouseDownEvent);
+screen.addEventListener("mousemove", mouseMoveEvent);
+screen.addEventListener("mouseup", mouseUpEvent);
+// clear button
+document.querySelector(".clear").addEventListener("click", clearScreen);
 
 //functions
 function colorClickEvent(e) {
-    let color = e.target.getAttribute('data-color');
-    currentColor = color;
+  let color = e.target.getAttribute("data-color");// checking which color was clicked
+  currentColor = color;
 
-    document.querySelector('.color.active').classList.remove('active');
-    e.target.classList.add('active');
+  document.querySelector(".color.active").classList.remove("active"); // removing the color that is checked by default
+  e.target.classList.add("active"); // adding which color you clicked on
 }
 
-function mouseDownEvent(e) { // Ativa o modo de desenho e Captura as coordenadas exatas do mouse dentro da tela de desenho 
-    canDraw = true; 
-    mouseX = e.pageX - screen.offsetLeft;
-    mouseY = e.pageY - screen.offsetTop;
-
-
+function mouseDownEvent(e) {
+ // Activate drawing mode and capture the exact mouse coordinates within the drawing canvas
+  canDraw = true;
+  mouseX = e.pageX - screen.offsetLeft;
+  mouseY = e.pageY - screen.offsetTop;
 }
 
 function mouseMoveEvent(e) {
-if(canDraw) {
-   draw(e.pageX, e.pageY )
-
-}
-
+  if (canDraw) {
+    draw(e.pageX, e.pageY); // mouse position
+  }
 }
 
 function mouseUpEvent() {
-    canDraw = false;
+  canDraw = false;
 }
 
 function draw(x, y) {
-    let pointX = x - screen.offsetLeft;
-    let pointY = y - screen.offsetTop;
+  let pointX = x - screen.offsetLeft; 
+  let pointY = y - screen.offsetTop; 
 
-    ctx.beginPath();
-    ctx.lineWidth = 5;
-    ctx.lineJoin = "round";
-    ctx.moveTo(mouseX, mouseY);
-    ctx.lineTo(pointX, pointY);
-    ctx.closePath();
-    ctx.strokeStyle = currentColor;
-    ctx.stroke();
+// to design
+  ctx.beginPath(); // Starts a new drawing path.
+  ctx.lineWidth = 5; // line width
+  ctx.lineJoin = "round"; // shape(ball)
+  ctx.moveTo(mouseX, mouseY); // move the cursor (start position)
+  ctx.lineTo(pointX, pointY);  // Draw a line to the point
+  ctx.closePath(); // closing drawing process
+  ctx.strokeStyle = currentColor; // color
+  ctx.stroke(); // Draw the line with the defined width
 
-    mouseX = pointX;
-    mouseY = pointY;
+ // continuous drawing independent of position
+  mouseX = pointX;
+  mouseY = pointY;
 }
 
 function clearScreen() {
-    ctx.setTransform(1, 0, 0, 1, 0, 0)
-    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+  ctx.setTransform(1, 0, 0, 1, 0, 0); // 2d matrix
+  ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height); // clear to the end of the screen
 }
-
